5603372fc21940cd16ea35c040e955f096450176
Dayron <dayronp9408@gmail.com>
2025-09-22 01:08:47 -0400

    cambios redireccionamiento eliminados -- no necesarios

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // 👈 importante para rutas en Render
   plugins: [tailwindcss(), react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "index.html",
-      },
-    },
-  },
 });
